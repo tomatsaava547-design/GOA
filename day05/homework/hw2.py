acc9a7d2be1c594aedf1086503a0abbd939eb7b4
tomatsaava547-design <tomatsaava547@gmail.com>
2025-11-05 18:33:40 +0400

    second commit

---
+++ day05/homework/hw2.py
@@ -0,0 +1,9 @@
+#მომხარებელს შემოატანინეთ ორი რიცხვი, შემდეგ ამ 2 რიცხვს შორის ჩაატარეთ ყველა არითმეტიკული ოპერატორი (+, -, *, /, **)
+number1 =  1100
+number2 = 300
+print(number1-number2)
+print(number1+number2)
+print(number1*number2)
+print(number1/number2)
+print(number1**number2)
+print(number1//number2)
