82643f4015b19b57e44a565ff374769a321e88c7
tomatsaava547-design <tomatsaava547@gmail.com>
2025-11-14 17:59:32 +0400

    second commit

--- day04/homework/hw5.py
+++ day04/homework/hw5.py
@@ -1,5 +1,4 @@
 #შექმენით 5 ცვლადი name,age,surname,city,hight.name-ში შეეკითხეს სახელი,age-ში ასაკი,surname-ში გვარი,city-ში ქალაქი ხოლო hight-ში სიმაღლე.ბოლოს გამოიტანეთ ეკრანზე ასეთი ინფორმაცია "მე მქვია (თქვენი სახელი), (თქვენი გვარი)
-
 Name = input("enter your name")
-Surname = input ("enter your surname")
-print(" მე მქვია" + Name +","+ Surname)
+surname= input("enter your name")
+print(Name + surname)
