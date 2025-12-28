7b513181411c4d74e01e04e16d1e1a7eb8739fa2
tomatsaava547-design <tomatsaava547@gmail.com>
2025-12-28 22:44:43 +0400

    second commit

---
+++ GOA.py
@@ -0,0 +1,24 @@
+import turtle
+
+
+screen = turtle.Screen()
+screen.bgcolor("white")
+
+
+t = turtle.Turtle()
+t.speed(10)
+t.color("black" )
+t.pensize(5)
+
+
+t.penup()
+t.goto(0, -50)   
+t.pendown()
+t.circle(100)
+
+
+t.penup()
+t.goto(-100,0)
+
+
+
---
+++ gamocda/bb/shaurma.py
@@ -0,0 +1,30 @@
+#  TASK 2-Store the sum of two numbers a and b in a variable result
+
+# a = 2
+# b = 3
+# result = a + b
+# # print(result)
+
+#TASK 10- Use a while loop to count from 1 to 5 and print all of the numbers in the terminal.
+
+# num = 0
+
+# while num < 5:
+#     num = num + 1
+#     print(num)
+
+# TASK 6-Given two boolean values a and b, store the result of a or b in result.
+
+# a = False
+# b = True
+
+# result = a + b
+
+# print(result )
+
+
+# TASK 12-Use a for loop with range() to calculate the sum of all even numbers from 1 to 10 and store it in result.
+# total=0
+# for i in range(0, 12, 2):
+#     total+=i
+# print(total)
